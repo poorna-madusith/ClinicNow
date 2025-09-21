e39e823112741fb9c4dc0bf3e2aa03c6524dc994
Poorna Madusith <157389469+poorna-madusith@users.noreply.github.com>
2025-09-21 14:06:23 +0530

    add settings classes for secrets

---
+++ backend/Settings/GoogleAuthSettings.cs
@@ -0,0 +1,7 @@
+namespace backend.Settings;
+
+public class GoogleAuthSettings
+{ 
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
+}
---
+++ backend/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+
+namespace backend.Settings;
+public class JwtSettings
+{
+    public string key { get; set; } = null!;
+    public string issuer { get; set; } = null!;
+    public string audience { get; set; } = null!;
+    public double durationInMinutes { get; set; }
+}
