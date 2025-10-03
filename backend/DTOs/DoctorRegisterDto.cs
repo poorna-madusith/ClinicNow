8a514afadf9999818bb0bd51a70705511bad2c37
Poorna Madusith <157389469+poorna-madusith@users.noreply.github.com>
2025-10-03 08:07:57 +0530

    created doctor register DTO and register service

---
+++ backend/DTOs/DoctorRegisterDto.cs
@@ -0,0 +1,49 @@
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using backend.Models;
+
+namespace backend.DTOs;
+
+public class DoctorRegisterDto
+{
+    [Required(ErrorMessage = "First Name is required")]
+
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last Name is required")]
+    public string LastName { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; } = null!;
+
+    [Range(0, 120, ErrorMessage = "Age must be between 0 and 120")]
+    public int? Age { get; set; }
+
+    [EnumDataType(typeof(GenderEnum), ErrorMessage = "Invalid Gender")]
+    public GenderEnum? Gender { get; set; }
+
+    [Required(ErrorMessage = "Specialization is required")]
+    public string? Specialization { get; set; }
+
+    [Required(ErrorMessage = "Doctor Description is required")]
+    public string? DocDescription { get; set; }
+
+    [Required(ErrorMessage = "Profile Image URL is required")]
+    public string? ProfileImageUrl { get; set; }
+    [Required(ErrorMessage = "Contact Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Contact Email Address")]
+    public string? ContactEmail { get; set; }
+
+    [Required(ErrorMessage = "At least one Contact Number is required")]
+    [MinLength(1, ErrorMessage = "At least one Contact Number is required")]
+    public int[]? ContactNumbers { get; set; }
+
+    [Required(ErrorMessage = "Address is required")]
+    public string? Address { get; set; }
+    
+}
---
+++ backend/Services/AdminDocServices.cs
@@ -0,0 +1,59 @@
+using backend.Data;
+using backend.DTOs;
+using backend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Services;
+
+
+
+public class AdminDocServices
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ApplicationDBContext _context;
+
+    public AdminDocServices(UserManager<ApplicationUser> userManager, ApplicationDBContext context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+
+    public async Task<IActionResult> CreateDoctor(DoctorRegisterDto doctorRegisterDto)
+    {
+        var existingDoc = await _userManager.FindByEmailAsync(doctorRegisterDto.Email);
+        if (existingDoc != null)
+        {
+            throw new Exception("Doctor with this email already exists");
+        }
+
+        var doctor = new ApplicationUser
+        {
+            FirstName = doctorRegisterDto.FirstName,
+            LastName = doctorRegisterDto.LastName,
+            Email = doctorRegisterDto.Email,
+            UserName = doctorRegisterDto.Email,
+            Role = RoleEnum.Doctor,
+            Age = doctorRegisterDto.Age,
+            Gender = doctorRegisterDto.Gender,
+            Specialization = doctorRegisterDto.Specialization,
+            DocDescription = doctorRegisterDto.DocDescription,
+            ProfileImageUrl = doctorRegisterDto.ProfileImageUrl,
+            ContactEmail = doctorRegisterDto.ContactEmail,
+            ContactNumbers = doctorRegisterDto.ContactNumbers,
+            Address = doctorRegisterDto.Address
+        };
+
+        var result = await _userManager.CreateAsync(doctor, doctorRegisterDto.Password);
+        if (!result.Succeeded)
+        {
+            throw new Exception(string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+        await _userManager.AddToRoleAsync(doctor, doctor.Role.ToString());
+        return new OkObjectResult("Doctor created successfully");
+        
+    }
+    
+}
